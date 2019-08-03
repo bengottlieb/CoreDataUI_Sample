82023127309e0f2baf2f9c778867c963aba602a3
Ben Gottlieb <ben@standalone.com>
2019-08-03 06:22:09 -0500

    Initial Commit

---
+++ CoreDataUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CoreDataUI
+//
+//  Created by ben on 8/3/19.
+//  Copyright © 2019 Stand Alone, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
